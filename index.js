86c5be327d717c5d54d1f4d5e2e2c2a081d2c602
thongdeptraiwa <thong442001@gmail.com>
2024-09-11 20:02:50 +0700

    swagger9

--- index.js
+++ index.js
@@ -28,21 +28,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-config.js');
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-config.js');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //connect database
 mongoose.connect('mongodb+srv://thong442001:F3WK9R2BOb3cV86h@totnghiep.8wwlj.mongodb.net/totNghiep')//link connect vs mongobd
     .then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
     .catch(err => console.log('>>>>>>>>> DB Error: ', err));
 
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 //mogo
 app.use('/user', userRoute);
 app.use('/post', postRoute);
