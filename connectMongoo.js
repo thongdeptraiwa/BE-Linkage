2896498dcff2d0e15598cdae31f11df0288a86aa
thongdeptraiwa <thong442001@gmail.com>
2024-09-11 18:02:19 +0700

    t7

--- connectMongoo.js
+++ connectMongoo.js
@@ -5,7 +5,7 @@ require("./models/friendNotification");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://thong442001:F3WK9R2BOb3cV86h@totnghiep.8wwlj.mongodb.net/totNghiep')//link connect vs mongobd
+        await mongoose.connect('mongodb+srv://thong442001:F3WK9R2BOb3cV86h@totnghiep.8wwlj.mongodb.net/totNghiep?retryWrites=true&w=majority&appName=totNghiep')//link connect vs mongobd
         // .then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
         // .catch(err => console.log('>>>>>>>>> DB Error: ', err));
         console.log('>>>>>>>>>> DB Connected!!!!!!')
