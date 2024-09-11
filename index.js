b3e9b0edcc2b8ae716ae5eebd1401a44ddfe0d62
thongdeptraiwa <thong442001@gmail.com>
2024-09-11 17:13:00 +0700

    t2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
+//var createError = require('http-errors');
 const express = require("express");
-var path = require('path');
+//var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
 
 //config mongoose
 const mongoose = require("mongoose");
@@ -18,11 +18,11 @@ var friendNotificationRoute = require('./routes/friendNotificationRoute');
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 //swagger
 const swaggerUi = require('swagger-ui-express');
@@ -43,20 +43,20 @@ app.use('/post', postRoute);
 app.use('/friendNotification', friendNotificationRoute);
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 //app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
